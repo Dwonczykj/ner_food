a427261e4d0a7c5791d50378cfcd3a17363530a2
Joe Dwonczyk <JDwonczyk@gmail.com>
2021-12-27 20:56:44 +0000

    Add Url_Categorizer empty file

---
+++ notebooks/url_categorizer.py
@@ -0,0 +1,17 @@
+# workout how to save a url embedding, pickle? -> fast to write to, slow to read, easy to store in a db.
+#   tojson() ?
+
+# Find each different path / query category in the tree automatically, using regex patterns? / tree depth etc when traversing the tree. 
+#   Start by taking a simple printout of a tree with muytlipe url types from a unit test.
+
+# For each url TEXT Path in the url embedding, categroise the category of urls by checking the rankpair for 
+#   that subtree (ensure can calculate rankpair of subtreenodes too w/ tests).
+
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
